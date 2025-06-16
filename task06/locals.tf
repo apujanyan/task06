a5ede6f2d3045527e77f0340e3d73ddc69a8987d
Aramazd Apujanyan <apujanyan04@gmail.com>
2025-06-17 00:29:16 +0400

    ...

--- task06/locals.tf
+++ task06/locals.tf
@@ -1,9 +1,9 @@
 locals {
-  rg_name         = "${var.resources_name_prefix}-rg"
-  sql_server_name = "${var.resources_name_prefix}-sqlsrv"
-  sql_db_name     = "${var.resources_name_prefix}-sqldb"
-  asp_name        = "${var.resources_name_prefix}-asp"
-  app_name        = "${var.resources_name_prefix}-app"
+  rg_name         = join("-", [var.resources_name_prefix, "rg"])
+  sql_server_name = join("-", [var.resources_name_prefix, "sqlsrv"])
+  sql_db_name     = join("-", [var.resources_name_prefix, "sqldb"])
+  asp_name        = join("-", [var.resources_name_prefix, "asp"])
+  app_name        = join("-", [var.resources_name_prefix, "app"])
   tags = {
     Creator = var.student_email
   }
