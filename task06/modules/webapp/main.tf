4716f2c4526952fc9c1fbf213a9be41e2af9cb80
Aramazd Apujanyan <apujanyan04@gmail.com>
2025-06-17 22:37:09 +0400

    ...

--- task06/modules/webapp/main.tf
+++ task06/modules/webapp/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_linux_web_app" "app" {
   name                = var.app_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  service_plan_id     = azurerm_app_service_plan.asp.id
+  service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
     application_stack {
