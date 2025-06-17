9f1d59879eb89c51e77a9f483c1243e21dffbce1
Aramazd Apujanyan <apujanyan04@gmail.com>
2025-06-17 22:33:02 +0400

    ...

--- task06/modules/webapp/main.tf
+++ task06/modules/webapp/main.tf
@@ -1,11 +1,9 @@
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_service_plan" "asp" {
   name                = var.asp_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku {
-    tier = "Basic"
-    size = var.asp_sku
-  }
+  os_type = "Linux"
+  sku_name = var.asp_sku
   tags = var.tags
 }
 
