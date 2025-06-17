4e00e3b2d2fd6a7650b07cefe129a6ba21c7b4e0
Aramazd Apujanyan <apujanyan04@gmail.com>
2025-06-17 23:04:20 +0400

    ...

--- task06/modules/sql/main.tf
+++ task06/modules/sql/main.tf
@@ -14,7 +14,16 @@ resource "azurerm_mssql_server" "sql_server" {
   version                      = "12.0"
   administrator_login          = local.sql_admin_username
   administrator_login_password = random_password.sql_password.result
+  minimum_tls_version          = "1.2"
   tags                         = var.tags
+}
+
+resource "azurerm_mssql_database" "sql_db" {
+  name       = var.sql_db_name
+  server_id  = azurerm_mssql_server.sql_server.id
+  sku_name   = var.sql_sku
+  tags       = var.tags
+  depends_on = [azurerm_mssql_server.sql_server]
 }
 
 resource "azurerm_mssql_firewall_rule" "allow_azure" {
@@ -22,6 +31,7 @@ resource "azurerm_mssql_firewall_rule" "allow_azure" {
   server_id        = azurerm_mssql_server.sql_server.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
+  depends_on       = [azurerm_mssql_database.sql_db]
 }
 
 resource "azurerm_mssql_firewall_rule" "allow_ip" {
@@ -29,23 +39,19 @@ resource "azurerm_mssql_firewall_rule" "allow_ip" {
   server_id        = azurerm_mssql_server.sql_server.id
   start_ip_address = var.allowed_ip_address
   end_ip_address   = var.allowed_ip_address
-}
-
-resource "azurerm_mssql_database" "sql_db" {
-  name      = var.sql_db_name
-  server_id = azurerm_mssql_server.sql_server.id
-  sku_name  = var.sql_sku
-  tags      = var.tags
+  depends_on       = [azurerm_mssql_database.sql_db]
 }
 
 resource "azurerm_key_vault_secret" "sql_admin_secret" {
   name         = var.sql_admin_secret_name
   value        = local.sql_admin_username
   key_vault_id = var.kv_id
+  depends_on   = [azurerm_mssql_server.sql_server]
 }
 
 resource "azurerm_key_vault_secret" "sql_password_secret" {
   name         = var.sql_admin_secret_password
   value        = random_password.sql_password.result
   key_vault_id = var.kv_id
+  depends_on   = [azurerm_mssql_server.sql_server]
 }
